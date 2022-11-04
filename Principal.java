b2dceebf01b8d053175dac41088d4297b6eee4cc
Iván González <ivan.gonzalez@campusfp.es>
2022-11-04 13:46:56 +0100

    first commit

---
+++ Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+    public static void main(String[] args) throws InterruptedException {
+        BufferExamenes generador = new BufferExamenes();
+        for (int i = 0; i < args.length; i++) {
+            new ProductorExamenes(generador);
+            new Examinador(args[i], generador);
+        }
+    }
+}
---
+++ Procesos.java
@@ -0,0 +1,28 @@
+
+import java.io.File;
+
+public class Procesos {
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder("java", "Principal", "Alumno1", "Alumno2", "Alumno3");
+
+        pb.redirectOutput(new File("salida1.txt"));
+        pb.redirectError(new File("error1.txt"));
+        try {
+            Process p = pb.start();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ProcessBuilder pb2= new ProcessBuilder("java", "Principal", "Rosa", "Miguel", "Pedro");
+        pb2.redirectOutput(new File("salida2.txt"));
+        pb2.redirectError(new File("error2.txt"));
+        try {
+            Process p2 = pb2.start();
+            p2.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
