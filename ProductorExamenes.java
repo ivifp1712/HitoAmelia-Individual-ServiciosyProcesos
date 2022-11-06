e887a9294c029262635fa158e1f6e2d65b99e730
ivi1712 <ivi1712@hotmail.es>
2022-11-06 17:29:07 +0100

    second commit

---
+++ ProductorExamenes.java
@@ -0,0 +1,35 @@
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class ProductorExamenes implements Runnable {
+    private BufferExamenes buffer;
+    private static int numeroExamen = 0;
+    private Thread hilo;
+
+    public ProductorExamenes(BufferExamenes buffer) {
+        // Incrementa el contador de exámenes (variable numeroExamen).
+        this.numeroExamen++;
+        // Construye el hilo. El nombre será la letra E seguida
+        // del valor de la variable numeroExamen.
+        this.hilo = new Thread(this, "E" + numeroExamen);
+        // Establece el valor de la propiedad buffer
+        this.buffer = buffer;
+        // Inicia el hilo.
+        hilo.start();
+    }
+
+    @Override
+    public void run() {
+        int aa = LocalDateTime.now().getYear();
+        // Generación del código de examen.
+        String codigo = this.hilo.getName() + "-" +aa;
+
+        // Añade el nuevo código al buffer de exámenes.
+        this.buffer.fabricarNuevoExamen(codigo);
+        // Muestra un mensaje en consola informando sobre el
+        // código del examen que se acaba de producir.
+        System.out.println("Se ha producido el examen " + codigo);
+
+    }
+}
